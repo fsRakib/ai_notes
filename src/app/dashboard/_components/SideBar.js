178ba02752141b95d7897b6487917f2db6a8d1f1
fsRakib <fsrakiba50@gmail.com>
2025-05-07 15:38:09 +0600

    bugfix/Rakib/Sidebar fixed

--- src/app/dashboard/_components/SideBar.js
+++ src/app/dashboard/_components/SideBar.js
@@ -8,16 +8,19 @@ import UploadPdfDialog from "./UploadPdfDialog";
 import { useUser } from "@clerk/nextjs";
 import { useQuery } from "convex/react";
 import { api } from "../../../../convex/_generated/api";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
 
 function SideBar() {
   const { user } = useUser();
 
+  const path = usePathname();
   const fileList = useQuery(api.fileStorage.GetUserFiles, {
     userEmail: user?.primaryEmailAddress?.emailAddress,
   });
   return (
     <div className="shadow-md h-screen  p-7">
-      <div className="bg-gray-700 w-full justify-center flex">
+      <div className=" w-full justify-center flex">
         <Image src="/logo.png" width={120} height={120} alt="logo" />
       </div>
 
@@ -26,15 +29,22 @@ function SideBar() {
           <Button className="w-full">+Upload Pdf</Button>
         </UploadPdfDialog>
       </div>
-
-      <div className="flex gap-2 items-center p-3 mt-5 hover:bg-slate-100 cursor-pointer">
-        <Layout />
-        <h2>Workspace</h2>
-      </div>
-      <div className="flex gap-2 items-center p-3 mt-1 hover:bg-slate-100 cursor-pointer">
-        <Shield />
-        <h2>Upgrade</h2>
-      </div>
+      <Link href="/dashboard">
+        <div
+          className={`flex gap-2 items-center p-3 mt-5 hover:bg-slate-100 cursor-pointer ${path == "/dashboard" && "bg-slate-200"}`}
+        >
+          <Layout />
+          <h2>Workspace</h2>
+        </div>
+      </Link>
+      <Link href="/dashboard/upgrade">
+        <div
+          className={`flex gap-2 items-center p-3 mt-1 hover:bg-slate-100 cursor-pointer ${path == "/dashboard/upgrade" && "bg-slate-200"}`}
+        >
+          <Shield />
+          <h2>Upgrade</h2>
+        </div>
+      </Link>
       <div className="absolute bottom-24 w-[80%]">
         <Progress value={(fileList?.length / 5) * 100} />
         <p className="text-sm mt-1">{fileList?.length} out of 5 Pdf Uploaded</p>
