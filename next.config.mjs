8b2dd652539219e3d2ac61233d61d9bc1499e90e
fsRakib <fsrakiba50@gmail.com>
2025-07-07 12:22:42 +0600

    Merge branch 'master' of https://github.com/fsRakib/ai_notes

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,16 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "img.clerk.com",
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
