07608862a676a7b2148faf33bd024f7bbafc682e
fsRakib <fsrakiba50@gmail.com>
2025-07-07 12:57:02 +0600

    Deploy on Azure

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,6 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "img.clerk.com",
-//       },
-//     ],
-//   },
-// };
-
-// export default nextConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   images: {
     remotePatterns: [
       {
@@ -20,6 +8,37 @@ const nextConfig = {
         hostname: "img.clerk.com",
       },
     ],
+    formats: ["image/webp"],
+    minimumCacheTTL: 60,
+  },
+  reactStrictMode: true,
+
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: `
+              default-src 'self';
+              script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.clerk.dev https://*.clerk.accounts.dev;
+              style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+              img-src 'self' https://img.clerk.com data:;
+              font-src 'self' https://fonts.gstatic.com data:;
+              connect-src *;
+              frame-src *;
+            `
+              .replace(/\s{2,}/g, " ")
+              .trim(),
+          },
+        ],
+      },
+    ];
   },
 };
 
